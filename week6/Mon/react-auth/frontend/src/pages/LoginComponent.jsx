85d7ef68ede31feb32f82be5b198f02bb7a09829
atinhutlk <nhut.vo@metropolia.fi>
2025-11-29 10:50:54 +0200

    update

---
+++ week6/Mon/react-auth/frontend/src/pages/LoginComponent.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const LoginComponent = ({ setIsAuthenticated }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+      try {
+      const response = await fetch("/api/user/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        alert(data.error || "Login failed");
+        return;
+      }
+
+      sessionStorage.setItem("user", JSON.stringify(data));
+
+    
+      setIsAuthenticated(true);
+
+      navigate("/");
+    } catch (error) {
+      console.error("Error during login:", error);
+      alert("Network error during login");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      <button onClick={handleLogin}>Log In</button>
+    </div>
+  );
+};
+
+export default LoginComponent;
